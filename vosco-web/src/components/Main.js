83e1189489fa81c86861e63ba96289990fb9c286
Yursleyzack <118051330+Yursleyzack@users.noreply.github.com>
2024-07-15 23:27:29 +0400

    trying to fix things

--- vosco-web/src/components/Main.js
+++ vosco-web/src/components/Main.js
@@ -5,7 +5,6 @@ export default function Main() {
     <div className="Main">
       <div>Bla Bla Bla</div>
       <div className="Division1">
-        <img></img>
         <div className="TextDiv">
           <span className="Text">Div 1 text</span>
         </div>
