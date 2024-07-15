0fdef50bd67c57f6d74a2512da9f663017b5a989
Yursleyzack <118051330+Yursleyzack@users.noreply.github.com>
2024-07-15 22:55:37 +0400

    Lol

---
+++ vosco-web/src/components/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <div className="Main">
+      <div>Bla Bla Bla</div>
+      <div className="Division1">
+        <img></img>
+        <div className="TextDiv">
+          <span className="Text">Div 1 text</span>
+        </div>
+      </div>
+    </div>
+  );
+}
